9b660646af8359ffcab26320d460206f0c1287ec
Marcos Vinicius <marcosvinicius.mecatronica@gmail.com>
2022-03-05 14:41:20 -0400

    feature: modify list component

--- ongs-do-bem/src/app/app-routing.module.ts
+++ ongs-do-bem/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
+import { LoginComponent } from './modules/login/login.component';
+import { ListComponent } from './modules/list/list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ListComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '**',
+    component: ListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ongs-do-bem/src/app/app.module.ts
+++ ongs-do-bem/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './modules/list/list.component';
+import { CaseComponent } from './modules/case/case.component';
+import { ProjectComponent } from './modules/project/project.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { LoginComponent } from './modules/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    CaseComponent,
+    ProjectComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
